5cdac349a550bde634727f808dbe4e03630932dd
anas-enn <ennassiri.anas1@gmail.com>
2025-12-13 20:48:58 +0100

    Update methods.js

--- methods.js
+++ methods.js
@@ -1,10 +1,10 @@
 async function getFromOpentdb(max = 100) {
     try {
-
         const response = await fetch(
-            `https://opentdb.com/api.php?amount=${max}`);
+            `https://opentdb.com/api.php?amount=${max}`
+        );
         // 'https://opentdb.com/api.php?amount=10&category=9&difficulty=easy&type=multiple');
-        //  'https://opentdb.com/api.php?amount=10&type=multiple');
+        // 'https://opentdb.com/api.php?amount=10&type=multiple');
         const loadedQuestions = await response.json();
         playWith(loadedQuestions.results);
     } catch (error) {
@@ -12,15 +12,18 @@ async function getFromOpentdb(max = 100) {
     }
 }
 
+function decodeHTML(html) {
+    const txt = document.createElement("textarea");
+    txt.innerHTML = html;
+    return txt.value;
+}
 
 function playWith(quizz) {
+    console.table(quizz);
 
-    console.table(quizz)
-
-    for (let {question} of quizz ) {
-        document.body.insertAdjacentHTML("beforeend", `<p>${question}</p>`)
-     }
-
+    for (let { question } of quizz) {
+        document.body.insertAdjacentHTML("beforeend", `<p>${decodeHTML(question)}</p>`);
+    }
 }
 
 getFromOpentdb(10);
